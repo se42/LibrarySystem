8cee634d1094be008334693de15b54d2acff7bde
Scott Edwards <scott.edwards3@gmail.com>
2016-05-17 15:33:04 -0400

    playing around with Title/Item construction

--- Item.py
+++ Item.py
@@ -1,11 +1,19 @@
 import datetime
 
 
+def new_title(title, author, value, item_type, *args, **kwargs):
+	d = {
+		'book': Book,
+		'av': AudioVideo,
+		'magazine': Magazine,
+	}
+	return d[item_type](title, author, value, *args, **kwargs)
+
+
 class Title:
 	"""docstring for Title"""
 	def __init__(self, title, author, value, edition=None, pub_date=None,
-		publisher=None, item_type='book', num_items=0, loanable=True,
-		bestseller=False, period=datetime.timedelta(weeks=3)):
+		publisher=None, num_items=0):
 
 		self.title = title
 		self.author = author
@@ -13,19 +21,32 @@ class Title:
 		self.edition = edition
 		self.pub_date = pub_date
 		self.publisher = publisher
-		self.loanable = loanable
-		self.bestseller = bestseller
-		self.period = period
+		self.loanable = True
+		self.bestseller = False
+		self.period = datetime.timedelta(weeks=3)
 		self.items = []
 
 		for each in range(num_items):
-			if item_type == 'book':
-				x = Book(self)
-			elif item_type == 'audiovideo':
-				x = AudioVideo(self)
-			elif item_type == 'magazine':
-				x = Magazine(self)
-			self.items.append(x)
+			self.items.append(Item(self))
+
+
+class Book(Title):
+	"""docstring for Book"""
+	def __init__(self, *args, **kwargs):
+		super(Book, self).__init__(*args, **kwargs)
+
+
+class AudioVideo(Title):
+	"""docstring for AudioVideo"""
+	def __init__(self, *args, **kwargs):
+		super(AudioVideo, self).__init__(*args, **kwargs)
+
+
+class Magazine(Title):
+	"""docstring for Magazine"""
+	def __init__(self, *args, **kwargs):
+		super(Magazine, self).__init__(*args, **kwargs)
+		self.loanable = False
 
 
 class Item:
@@ -42,24 +63,6 @@ class Item:
 		num = max(cls._item_numbers) + 1
 		cls._item_numbers.append(num)
 		return num
-
-
-class Book(Item):
-	"""docstring for Book"""
-	def __init__(self, *args, **kwargs):
-		super(Book, self).__init__(*args, **kwargs)
-
-
-class AudioVideo(Item):
-	"""docstring for AudioVideo"""
-	def __init__(self, *args, **kwargs):
-		super(AudioVideo, self).__init__(*args, **kwargs)
-
-
-class Magazine(Item):
-	"""docstring for Magazine"""
-	def __init__(self, *args, **kwargs):
-		super(Magazine, self).__init__(*args, **kwargs)
 		
 		
 		
